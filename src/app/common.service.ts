c1a04989de11df9dafc32c5045e6e28a0d7f28ad
Ruy Kaarlo <ruy.kaarlo@wozingagroup.com>
2022-09-17 03:42:35 +0800

    signup added

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -5,13 +5,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CommonService {
-  // host_url = 'http://localhost:3000';
+  localhost_url = 'http://localhost:3000';
   host_url = 'https://my-json-server.typicode.com/kaarlosasiang/codebyters';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   loginUser(path: string) {
     return this.http.get(this.host_url + path);
+  }
+
+  // registerUser(path: string, details: {}) {
+  //   this.registerUserlocally(path, details);
+  //   return this.http.post(this.host_url + path, details);
+  // }
+
+  registerUser(path: string, details: {}) {
+    return this.http.post(this.localhost_url + path, details)
   }
 
   checkLogin() {
--- src/app/user-registry/register/register.component.ts
+++ src/app/user-registry/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonService } from 'src/app/common.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newUSerModel = {
+    "id": 0,
+    "firstname": "",
+    "lastname": "",
+    "email": "",
+    "password": "",
+    "node": 0,
+    "role": 0,
+    "points": 0,
+    "achievements": []
+  }
+
+  constructor(private commonService: CommonService) { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(data: any) {
+    const path = "/users";
+    let currentID = 0;
+    if (data.firstname !== "" && data.lastname !== "" && data.email !== "" && data.password !== "" && data.re_enter_password !== "") {
+      if (data.password === data.re_enter_password) {
+        this.commonService.loginUser("/users").subscribe((res: any) => {
+          for (let i = 0; i < res.length; i++) {
+            if (res[i].id > currentID) {
+              currentID++;
+            }
+          }
+
+          this.newUSerModel = {
+            "id": currentID + 1,
+            "firstname": data.firstname,
+            "lastname": data.lastname,
+            "email": data.email,
+            "password": data.password,
+            "node": 0,
+            "role": 0,
+            "points": 0,
+            "achievements": []
+          };
+          console.log(this.newUSerModel);
+          this.commonService.registerUser(path, this.newUSerModel).subscribe((res: any) => {
+            console.log(res);
+          })
+        })
+
+
+
+
+
+      } else {
+        alert("Password incorrect!")
+      }
+
+    } else {
+      console.log("empty");
+
+    }
   }
 
 }
